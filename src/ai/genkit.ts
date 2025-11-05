0cc9f986a826a608876fc8ce60720ca901e9f761
Antonio Cristian Regina Puccini <cristianregina13@gmail.com>
2025-11-05 21:38:39 +0000

    Try fixing this error: `Console Error: TypeError: loadedPlugin.initializ

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,11 +1,11 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
-import * as nextPlugin from '@genkit-ai/next';
+import nextPlugin from '@genkit-ai/next';
 
 export const ai = genkit({
   plugins: [
     googleAI(),
-    nextPlugin.default({
+    nextPlugin({
       // We are mounting the API routes under /api/genkit.
       // This is the default.
       apiMountPath: '/api/genkit',
