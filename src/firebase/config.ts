ad7f31af2eb90089b29dca8ca79e7a65ee83e9ea
Antonio Cristian Regina Puccini <cristianregina13@gmail.com>
2025-11-05 10:30:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7773485527-bae7a",
+  "appId": "1:753137218141:web:bead85886c1087a50693b4",
+  "apiKey": "AIzaSyDKVI81ma4qG57zEjlRcpbicrWj-wa7GaI",
+  "authDomain": "studio-7773485527-bae7a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "753137218141"
+};
