1edb301f196795d31c10472185fd04d75043709f
Crisanfitos <cristianregina13@gmail.com>
2026-01-04 20:52:37 +0000

    Fix: Remove build artifacts and update .gitignore

--- client/.next/static/chunks/client_src_app_signup_page_tsx_05c2382e._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/add2e_zod_v4_c897e455._.js",
-  "static/chunks/_4edfea07._.js",
-  "static/chunks/client_src_61956c80._.js"
-],
-    source: "dynamic"
-});
